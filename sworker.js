bb674976af7d13d819fa2871caf91784bf4b6c22
ankithats <ankithats2@gmail.com>
2024-08-08 14:46:46 +0530

    main

---
+++ sworker.js
@@ -0,0 +1,15 @@
+self.addEventListener("install",e=>{
+    e.waitUntil(
+        caches.open("static").then(cache=>{
+            return cache.addAll(["./","./style.css","./images/logo64.png","./images/logo512.png"]);
+        })
+    )
+})
+
+self.addEventListener("fetch",e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    )
+})
